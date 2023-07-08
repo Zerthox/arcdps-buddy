3c0f3dfc2a426d40a6db8bc96b913e98dc24bb37
Zerthox <zerthox@gmail.com>
2023-07-08 18:54:05 +0200

    Add skill name overrides (#8)

--- src/combat/skill.rs
+++ src/combat/skill.rs
@@ -1,3 +1,5 @@
+use phf::phf_map;
+
 #[derive(Debug, Clone)]
 pub struct Skill {
     pub id: u32,
@@ -8,10 +10,17 @@ impl Skill {
     pub fn new(id: u32, name: Option<impl Into<String>>) -> Self {
         Self {
             id,
-            name: match name {
-                Some(name) => name.into(),
-                None => id.to_string(),
+            name: match OVERRIDES.get(&id) {
+                Some(name) => name.to_string(),
+                None => match name {
+                    Some(name) => name.into(),
+                    None => id.to_string(),
+                },
             },
         }
     }
 }
+
+static OVERRIDES: phf::Map<u32, &'static str> = phf_map! {
+    22492u32 => "Basilisk Venom",
+};
