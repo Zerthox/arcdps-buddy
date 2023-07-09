c47383eef66841ea852e304c7a7d91aa6611ef74
Zerthox <zerthox@gmail.com>
2023-07-10 01:08:44 +0200

    Track thief venoms & some slb stances

--- src/combat/buff.rs
+++ src/combat/buff.rs
@@ -32,6 +32,30 @@ pub enum Buff {
 
     #[strum(serialize = "Alac")]
     Alacrity,
+
+    #[strum(serialize = "Spider")]
+    SpiderVenom,
+
+    #[strum(serialize = "Skale")]
+    SkaleVenom,
+
+    #[strum(serialize = "Devo")]
+    DevourerVenom,
+
+    #[strum(serialize = "Basi")]
+    BasiliskVenom,
+
+    #[strum(serialize = "Soul Stone")]
+    SouleStoneVenom,
+
+    #[strum(serialize = "Moa")]
+    MoaStance,
+
+    #[strum(serialize = "Vult")]
+    VultureStance,
+
+    #[strum(serialize = "OWP")]
+    OneWolfPack,
 }
 
 impl TryFrom<u32> for Buff {
@@ -41,6 +65,14 @@ impl TryFrom<u32> for Buff {
         match value {
             1187 => Ok(Self::Quickness),
             30328 => Ok(Self::Alacrity),
+            13036 => Ok(Self::SpiderVenom),
+            13054 => Ok(Self::SkaleVenom),
+            13094 => Ok(Self::DevourerVenom),
+            13133 => Ok(Self::BasiliskVenom),
+            49038 => Ok(Self::SouleStoneVenom),
+            45038 => Ok(Self::MoaStance),
+            44651 => Ok(Self::VultureStance),
+            44139 => Ok(Self::OneWolfPack),
             value => Err(value),
         }
     }
