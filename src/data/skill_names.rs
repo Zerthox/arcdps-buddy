03ea03324c1de14960460cee79c107100cdc5edd
Zerthox <zerthox@gmail.com>
2024-05-04 14:33:58 +0200

    Add hunters verdict name override

--- src/data/skill_names.rs
+++ src/data/skill_names.rs
@@ -5,4 +5,5 @@ pub static SKILL_OVERRIDES: phf::Map<u32, &'static str> = phf_map! {
     12815u32 => "Lightning Leap Combo",
     22492u32 => "Basilisk Venom",
     31749u32 => "Blood Moon",
+    32410u32 => "Hunter's Verdict",
 };
